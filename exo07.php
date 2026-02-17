156f3ef2c8dcdb240d631bd3ae5cb19fc4b41bb4
Melichoux <meli.tournerie@gmail.com>
2026-02-17 16:56:33 +0100

    exo7 en cours

--- exo07.php
+++ exo07.php
@@ -31,11 +31,38 @@ $collection = [
 
     ];
 
-function afficherJeu($array, $index) {
-    foreach ($array[$index] as $key => $value) {
-            echo $key . ' : ' .$value.'<br>';
-        }
-    }
+echo "Afficher un jeu du tableau associatif<hr>";
+// function afficherJeu($array, $index) {
+//     foreach ($array[$index] as $key => $value) {
+//         if ($key=="scores") {
+//             echo "$key : ";
+//             for ($i=0; $i < count($value) ; $i++) { 
+//                 echo "$value[$i], ";
+//             };
+//         } else {
+//             echo $key . ' : ' .$value.'<br>';
+//         }
+//     }
+// }
 
-afficher
+// Alternative avec la fonction implode (transforme toute les valeurs d'un array avec un separateur de notre choix)
+// function afficherJeu($array, $index) {
+//     foreach ($array[$index] as $key => $value) {
+//         if ($key == "scores") {
+//             $scoresString = implode(", ", $value);  //ici le " ," defini le séparateur entre chaque valeur du array
+//             echo "scores : " . $scoresString . "<br>";
+//         } else {
+//             echo $key . ' : ' . $value . '<br>';
+//         }
+//     }
+// }
+
+// afficherJeu($collection, 1);
+
+echo "Calculer et retourner la moyenne des scores d'un jeu<hr>";
+
+function moyenneScore ($array) {
+    $moyenne = array_sum($array)/ count($array);
+    return $moyenne;
+}
 ?>
